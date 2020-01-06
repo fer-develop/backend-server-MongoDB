ecf3827cb3bd3798ff6cd8a383ea11392e9cbd9b
Alan <58962377+AlanFernandoProjects@users.noreply.github.com>
2020-01-05 19:24:43 -0700

    CRUD de usuarios y login token listo

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 // Rquieres - librerias para las funcionalidades que se van a ocupar
 var express = require('express');
+// Utilizar para definir el esquema que utiliza nuestros modelos para la insercion de datos
 var mongoose = require('mongoose');
+// Middleware que se encarga de convertir la data de un objeto es un archivo js para su insercion
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
 
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// mongoose.set('useCreateIndex', true);
+
+// Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 // Conexion a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err, res) => {
     if (err) throw err;
     console.log("Base de datos: \x1b[36m%s\x1b[0m", "Online");
 });
 
-// Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: "Peticion realizada correctamente"
-    });
-});
+//Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 //Escuchar peticiones
 app.listen(3000, () => {
---
+++ middlewares/autenticacion.js
@@ -0,0 +1,17 @@
+var jwt = require("jsonwebtoken");
+var SEED = require('../config/config').SEED;
+
+exports.verificaToken = function(req, res, next) {
+    var token = req.query.token;
+    jwt.verify(token, SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                mensaje: "Token Invalido",
+                errors: err
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
